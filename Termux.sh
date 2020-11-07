8a28adc5067fdf3842f629b0f17aeed3e2124d33
Moking001 <68329910+Moking001@users.noreply.github.com>
2020-11-07 21:19:26 +0430

    Add files via upload

---
+++ Termux.sh
@@ -0,0 +1,128 @@
+echo ""
+echo "#00ff3b"
+echo "MMMMMMMM               MMMMMMMM                 
+M:::::::M             M:::::::M                 
+M::::::::M           M::::::::M                 
+M:::::::::M         M:::::::::M                 
+M::::::::::M       M::::::::::M   ooooooooooo   
+M:::::::::::M     M:::::::::::M oo:::::::::::oo 
+M:::::::M::::M   M::::M:::::::Mo:::::::::::::::o
+M::::::M M::::M M::::M M::::::Mo:::::ooooo:::::o
+M::::::M  M::::M::::M  M::::::Mo::::o     o::::o
+M::::::M   M:::::::M   M::::::Mo::::o     o::::o
+M::::::M    M:::::M    M::::::Mo::::o     o::::o
+M::::::M     MMMMM     M::::::Mo::::o     o::::o
+M::::::M               M::::::Mo:::::ooooo:::::o
+M::::::M               M::::::Mo:::::::::::::::o
+M::::::M               M::::::M oo:::::::::::oo 
+MMMMMMMM               MMMMMMMM   oooooooooooo"
+echo "The script have lots of termux comends"
+echo ""
+echo "#ff0000"
+echo ""
+echo "subscribe  our telegram channel
+t.me/h4ker_moking"
+echo "#00ccff"
+echo ""
+echo ""
+echo "#ff0000" 
+echo ""
+echo ""
+echo ""
+
+pkg install python3
+pkg update -y                    
+pkg install python -y           
+pkg install python2 -y
+pkg install ruby -y 
+pkg install git -y
+pkg install screenfetch -y
+pkg install cowsay -y
+pkg install toilet -y
+pkg install figlet -y
+pkg install php -y 
+pkg install perl -y
+pkg install nmap -y
+pkg install openssh -y
+pkg install bash -y
+pkg install clang -y
+pkg install jq -y
+pkg install nano -y
+pkg install curl -y
+pkg install tar -y
+pkg install zip -y
+pkg install fish -y
+pkg install unzip -y
+pkg install tor -y 
+pkg install sudo -y
+pkg install wget -y
+pkg install tor -y
+pkg install w3m -y
+pkg install cmatrix -y
+pkg install wcalc -y 
+pkg install openssl -y
+pkg install bmon -y
+pkg upgrade -y
+echo ""
+echo ""
+echo ""
+echo ""
+
+echo "All basic packages and tools is installed"
+echo ""
+echo ""
+echo "
+                                                
+                                                
+MMMMMMMM               MMMMMMMM                 
+M:::::::M             M:::::::M                 
+M::::::::M           M::::::::M                 
+M:::::::::M         M:::::::::M                 
+M::::::::::M       M::::::::::M   ooooooooooo   
+M:::::::::::M     M:::::::::::M oo:::::::::::oo 
+M:::::::M::::M   M::::M:::::::Mo:::::::::::::::o
+M::::::M M::::M M::::M M::::::Mo:::::ooooo:::::o
+M::::::M  M::::M::::M  M::::::Mo::::o     o::::o
+M::::::M   M:::::::M   M::::::Mo::::o     o::::o
+M::::::M    M:::::M    M::::::Mo::::o     o::::o
+M::::::M     MMMMM     M::::::Mo::::o     o::::o
+M::::::M               M::::::Mo:::::ooooo:::::o
+M::::::M               M::::::Mo:::::::::::::::o
+M::::::M               M::::::M oo:::::::::::oo 
+MMMMMMMM               MMMMMMMM   oooooooooooo
+"
+echo "
+                                                                 
+                                                                 
+KKKKKKKKK    KKKKKKK  iiii                                       
+K:::::::K    K:::::K i::::i                                      
+K:::::::K    K:::::K  iiii                                       
+K:::::::K   K::::::K                                             
+KK::::::K  K:::::KKKiiiiiiinnnn  nnnnnnnn       ggggggggg   ggggg
+  K:::::K K:::::K   i:::::in:::nn::::::::nn    g:::::::::ggg::::g
+  K::::::K:::::K     i::::in::::::::::::::nn  g:::::::::::::::::g
+  K:::::::::::K      i::::inn:::::::::::::::ng::::::ggggg::::::gg
+  K:::::::::::K      i::::i  n:::::nnnn:::::ng:::::g     g:::::g 
+  K::::::K:::::K     i::::i  n::::n    n::::ng:::::g     g:::::g 
+  K:::::K K:::::K    i::::i  n::::n    n::::ng:::::g     g:::::g 
+KK::::::K  K:::::KKK i::::i  n::::n    n::::ng::::::g    g:::::g 
+K:::::::K   K::::::Ki::::::i n::::n    n::::ng:::::::ggggg:::::g 
+K:::::::K    K:::::Ki::::::i n::::n    n::::n g::::::::::::::::g 
+K:::::::K    K:::::Ki::::::i n::::n    n::::n  gg::::::::::::::g 
+KKKKKKKKK    KKKKKKKiiiiiiii nnnnnn    nnnnnn    gggggggg::::::g 
+                                                         g:::::g 
+                                             gggggg      g:::::g 
+                                             g:::::gg   gg:::::g 
+                                              g::::::ggg:::::::g 
+                                               gg:::::::::::::g  
+                                                 ggg::::::ggg    
+                                                    gggggg       
+"
+echo " telegram chanel  : t.me/h4ker_moking 
+youtube chanel Technical moking  "
+echo ""
+echo " this sceipt craeted by ❤🗣:moking
+from Technical moking join owr telegram channel t.me/h4ker_moking
+and subscribe owr YT chanel Technical moking"
+echo "     moking love you all"
+exit
